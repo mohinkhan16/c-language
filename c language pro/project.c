a1110692b4edb38e81c19516a1309f9f4edca26e
MohinKhan <mohinpathan2004@gmail.com>
2025-10-14 14:40:54 +0530

    Add files via upload

---
+++ c language pro/project.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    int main()
+    {
+        float firstAngle,secondAngle,thirdAngle;
+
+        firstAngle= 65;
+
+        secondAngle= 45;
+
+        thirdAngle=180-(firstAngle+secondAngle);
+
+        printf("thirdAngle:%f",thirdAngle);
+
+        return 0;
+    }
